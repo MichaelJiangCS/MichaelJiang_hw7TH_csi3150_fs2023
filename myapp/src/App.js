1119e174fa71418375b6736588eca67af0fe4509
Michael <michael.jiang799@gmail.com>
2023-11-19 00:56:16 -0500

    done

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import UserSignUp from './components/UserSignUp';
+
+function App() {
+  return (
+    <div className="App">
+     <UserSignUp />
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/components/UserSignUp.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+
+const UserSignUp = () => {
+  const [formData, setFormData] = useState({
+    fullName: '',
+    username: '',
+    email: '',
+    address: '',
+    phoneNumber: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('User Input:', formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Full Name:
+        <input
+          type="text"
+          name="fullName"
+          value={formData.fullName}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+
+      <label>
+        Desired Username:
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+
+      <label>
+        Address:
+        <input
+          type="text"
+          name="address"
+          value={formData.address}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+
+      <label>
+        Phone Number:
+        <input
+          type="tel"
+          name="phoneNumber"
+          value={formData.phoneNumber}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default UserSignUp;
